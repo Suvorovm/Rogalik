56565cb87230d24a75a8ce0549eeb36dbe8352fb
Максим Суворов <royming06@gmail.com>
2021-10-17 15:47:00 +0300

    feature(): Init Project

---
+++ Roguelike/Assets/Scripts/Core/Utils/ConsoleCommandFactory.cs
@@ -0,0 +1,16 @@
+﻿using IngameDebugConsole;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Core.Utils
+{
+    public class ConsoleCommandFactory : MonoBehaviour
+    {
+        [ConsoleMethod("sayGay", "Print message")]
+        [UsedImplicitly]
+        public static void SayGay()
+        {
+            Debug.LogWarning("You are GAY!!!");
+        }
+    }
+}
---
+++ Roguelike/Assets/Scripts/Roguelike/UI/TestDialog.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Roguelike.UI
+{
+    public class TestDialog : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _attackButton;
+        [SerializeField]
+        private Button _suckButton;
+
+        private void Awake()
+        {
+            _attackButton.onClick.AddListener(OnAttackButtonClick); 
+            _suckButton.onClick.AddListener(OnSuckButtonClick); 
+        }
+
+        private void OnSuckButtonClick()
+        {
+            Debug.LogWarning("Suck");
+        }
+
+        private void OnAttackButtonClick()
+        {
+            Debug.LogWarning("Attacked");
+        }
+        
+    }
+}
