1ae91fa8aa31d87d35628b7b74e76551070ba1e5
ArsPetr <arseniyp1@yandex.ru>
2021-10-21 12:25:39 +0300

    Игрок сохраняет поворот и поворачивается только при перемещении

--- Roguelike/Assets/Scripts/Roguelike/Player Controls/PlayerMovement.cs
+++ Roguelike/Assets/Scripts/Roguelike/Player Controls/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
 
     public Joystick RotateJoystick;
     public Joystick moveJoystick;
+
+    private Vector3 rotationVector;
     // Update is called once per frame
     private void Start()
     {
@@ -20,17 +22,33 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        // Поворот стиком
         float vertRotation = RotateJoystick.Vertical;
         float horizRotation = RotateJoystick.Horizontal;
         
+        // Передвижение стиком
         float horizontalJ = moveJoystick.Horizontal;
         float verticalJ = moveJoystick.Vertical;
 
-        Vector3 moveVector = (Vector3.up * horizRotation + Vector3.left * vertRotation);
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
-        
+        // Передвижение WASD
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        
+        if (vertRotation != 0 || horizRotation != 0)
+        {
+            rotationVector = (Vector3.up * horizRotation + Vector3.left * vertRotation);
+        }
+        else if (horizontalJ != 0 || verticalJ != 0)
+        {
+            rotationVector = (Vector3.up * horizontalJ + Vector3.left * verticalJ);
+        }
+        else if (horizontal != 0 || vertical != 0)
+        {
+            rotationVector = (Vector3.up * horizontal + Vector3.left * vertical);
+        }
+        
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, rotationVector);
+        
         Vector3 moveDirectionJ = new Vector3(horizontalJ, verticalJ);
         Vector3 moveDirection = new Vector3(horizontal, vertical);
         
