d9870cb2f23b19bb70d6d00f4e3595c7384e2c3e
l0narl0ser <ronyaufim@icloud.com>
2021-11-04 17:37:42 +0300

    Добавлена возможность паузы

--- Roguelike/Assets/Scripts/Roguelike/Fireball.cs
+++ Roguelike/Assets/Scripts/Roguelike/Fireball.cs
@@ -1,21 +1,21 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class FireballDamagingExplosion : MonoBehaviour
-{
-    public GameObject explosionEffect;
-    void OnTriggerEnter2D(Collider2D hitInfo )
-    {
-        Enemy enemy = hitInfo.GetComponent<Enemy>();
-        if (enemy != null)
-        {
-            Debug.Log("I blown up this fucker");
-        }
+//public class FireballDamagingExplosion : MonoBehaviour
+//{
+   // public GameObject explosionEffect;
+    //void OnTriggerEnter2D(Collider2D hitInfo )
+   // {
+       // Enemy enemy = hitInfo.GetComponent<Enemy>();
+        //if (enemy != null)
+        //{
+           // Debug.Log("I blown up this fucker");
+       // }
 
-        Instantiate(explosionEffect, transform.position, transform.rotation);
-        Destroy(gameObject);
-    }
+       // Instantiate(explosionEffect, transform.position, transform.rotation);
+       // Destroy(gameObject);
+    //}
     
-}
+//}
---
+++ Roguelike/Assets/Scripts/Roguelike/UI/PauseScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public void Pausegame()
+    {
+        if(Time.timeScale == 1)
+        Time.timeScale = 0;
+        else
+        Time.timeScale = 1;
+    }
+}
