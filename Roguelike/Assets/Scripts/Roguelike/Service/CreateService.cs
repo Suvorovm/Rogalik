55fc2c920ad22ae7f017db31583c3cff1c345b3a
ArsPetr <arseniyp1@yandex.ru>
2021-11-04 23:55:30 +0300

    Мелкие правки

--- Roguelike/Assets/Scripts/Roguelike/Service/CreateService.cs
+++ Roguelike/Assets/Scripts/Roguelike/Service/CreateService.cs
@@ -11,44 +11,45 @@ namespace Roguelike.Service
 {
     public class CreateService : MonoBehaviour
     {
-        
-        public void CreatePrefabObject( string reqObjectPath,[CanBeNull]string parentObject)
+        public void CreatePrefabObject(string reqObjectPath, [CanBeNull] string parentObject)
         {
-            AddGameObject(GetResource<GameObject>(reqObjectPath) , GetParentObjectByName(parentObject));
+            AddGameObject(GetResource<GameObject>(reqObjectPath), GetParentObjectByName(parentObject));
         }
 
         public void CreateObject(string reqObject, [CanBeNull] string parentObject)
         {
-            AddGameObject(GetReqObjectByName(reqObject),GetParentObjectByName(parentObject));
+            AddGameObject(GetReqObjectByName(reqObject), GetParentObjectByName(parentObject));
         }
+
         // Создаёт объект.
-        public void AddGameObject(GameObject gameObject,[CanBeNull] GameObject container = null, bool worldPositionStays = false)
+        public void AddGameObject(GameObject gameObject, [CanBeNull] GameObject container = null,
+            bool worldPositionStays = false)
         {
             Transform parentContainer = container == null ? transform : container.transform;
-            Instantiate(gameObject).transform.SetParent(parentContainer,worldPositionStays);
+            Instantiate(gameObject).transform.SetParent(parentContainer, worldPositionStays);
         }
-        
+
         // Находит родителя среди объектов на сцене.
         [CanBeNull]
         public GameObject GetParentObjectByName(string objectName)
         {
             return GetSceneObjects().FirstOrDefault(o => o.name == objectName);
         }
-        
+
         //Находит нужный объект среди объектов на сцене.
         public GameObject GetReqObjectByName(string objectName)
         {
             return GetSceneObjects().FirstOrDefault(o => o.name == objectName);
         }
-        
+
         //Возвращает лист всех объектов на сцене
         public List<GameObject> GetSceneObjects()
         {
             return gameObject.GetComponentsInChildren<Transform>(true).ToList().Select(t => t.gameObject).ToList();
         }
-        
+
         //Загружает префаб по заданному пути
-        public static T GetResource<T>(string prefabPath) where T : Object 
+        public static T GetResource<T>(string prefabPath) where T : Object
         {
             return Resources.Load<T>(prefabPath);
         }
--- Roguelike/Assets/Scripts/Roguelike/Service/HealthService.cs
+++ Roguelike/Assets/Scripts/Roguelike/Service/HealthService.cs
@@ -8,39 +8,39 @@ namespace Roguelike.Service
 {
     public class HealthService : MonoBehaviour
     {
-        [SerializeField]
-        private GameObject _otherGameObject;
+        [SerializeField] private GameObject _otherGameObject;
 
-        
-        
+
         private static int _maxHealth = 100;
         private int _health = _maxHealth;
-        
+
         private PlayerMovement _controls;
         private CreateService _crService;
 
         private HealthBar _healthBar;
-        
+
         // Путь к меню смерти
-        private const string _deathMenuPath = "UI/Game/DeathMenu/DeathMenu";
+        private const string DEATH_MENU_PATH = "UI/Game/DeathMenu/DeathMenu";
+
         // Название объекта parent
-        private const string _uiParentName = "MainScreen";
+        private const string UI_PARENT_NAME = "MainScreen";
+
         private void Awake()
         {
             _controls = _otherGameObject.GetComponent<PlayerMovement>();
             _crService = GameApplication.RequireService<CreateService>();
-            
+
             _healthBar = GetComponentInChildren<HealthBar>();
-            _healthBar.UpdateBar((float)_maxHealth/100);
-            
-            DebugLogConsole.AddCommand("kill","kills player",Death);
+            _healthBar.UpdateBar((float) _maxHealth / 100);
+
+            DebugLogConsole.AddCommand("kill", "kills player", Death);
         }
 
         public void IncreaseHealth(int health)
         {
             _health += health;
             if (_health > _maxHealth) _health = 100;
-            _healthBar.UpdateBar((float)_health/100);
+            _healthBar.UpdateBar((float) _health / 100);
         }
 
         public void DecreaseHealth(int health)
@@ -51,17 +51,16 @@ namespace Roguelike.Service
                 _health = 0;
                 Death();
             }
-            _healthBar.UpdateBar((float)_health/100);
-            
+
+            _healthBar.UpdateBar((float) _health / 100);
         }
 
-        
+
         void Death()
         {
             _controls.enabled = false;
-            Debug.Log("i dead");
-            _crService.CreatePrefabObject(_deathMenuPath,_uiParentName);
+            Debug.Log("i am dead");
+            _crService.CreatePrefabObject(DEATH_MENU_PATH, UI_PARENT_NAME);
         }
-        
     }
 }
