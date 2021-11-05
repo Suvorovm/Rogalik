568524010efc5162bd3e6b233c5cc5cea5fdf81b
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-11-06 00:11:14 +0300

    test(): тест прав доступа

--- Roguelike/Assets/Scripts/Roguelike/Fireball.cs
+++ Roguelike/Assets/Scripts/Roguelike/Fireball.cs
@@ -16,6 +16,9 @@ public class FireballDamagingExplosion : MonoBehaviour
 
         Instantiate(explosionEffect, transform.position, transform.rotation);
         Destroy(gameObject);
+
+
+        
     }
     
 }
