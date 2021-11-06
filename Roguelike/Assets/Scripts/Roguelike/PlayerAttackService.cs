f128f2c1304dfe87bff49ba14412997361980d21
Eg0rS <71844476+Eg0rS@users.noreply.github.com>
2021-11-06 18:57:50 +0300

    fix(): разрешение конфликтов

--- Roguelike/Assets/Scripts/Roguelike/PlayerAttackService.cs
+++ Roguelike/Assets/Scripts/Roguelike/PlayerAttackService.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class PlayerAttackService : MonoBehaviour
 {
     private PlayerCombat _playerCombat;
+
     public void Awake()
     {
         _playerCombat = gameObject.GetComponentInChildren<PlayerCombat>();
     }
+
     public void Attack()
     {
-        _playerCombat.Attack(); 
+        _playerCombat.Attack();
     }
 
     public void Shoot()
     {
-        _playerCombat.DistanceAttack(); 
+        _playerCombat.DistanceAttack();
     }
-    
 }
