ab8396ab22ca214e3dfc6c6914eb0e82280785e5
Aud3rk <sadykov.zaxar@mail.ru>
2021-11-14 22:14:19 +0300

    Добавил главное меню

---
+++ Roguelike/Assets/Scripts/Roguelike/UI/MainMenu/MainMenu.cs
@@ -0,0 +1,45 @@
+
+using Core.Utils;
+using UnityEngine;
+
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject _overlay;
+    private GameObject _mainMenu;
+    private GameObject MainScreen;
+    private GameObject _player;
+    void OnEnable()
+    {
+        _mainMenu = ResourseLoadService.GetResource<GameObject>("UI/Game/MainMenu/MainMenu");
+        MainScreen = GameObject.Find("MainScreen");
+        _mainMenu = Instantiate(_mainMenu);
+        _mainMenu.transform.SetParent(MainScreen.transform,false);
+            //1159.9  552.6
+    }
+
+    public void StartButton()
+    {
+        _mainMenu = GameObject.Find("MainMenu(Clone)");
+        _mainMenu.SetActive(false);
+        _player = ResourseLoadService.GetResource<GameObject>("GameWorld/2DObject/Player/Player");
+        GameObject _world = GameObject.Find("World");
+        _player = Instantiate(_player);
+        _player.transform.SetParent(_world.transform,false);
+        MainScreen = GameObject.Find("MainScreen");
+        _overlay = ResourseLoadService.GetResource<GameObject>("UI/Game/Overlay/pfGameOverlay");
+        Instantiate(_overlay).transform.SetParent(MainScreen.transform, false);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
