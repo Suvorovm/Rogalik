3b8fc04721b344d525d8d28d00a800e1ac69bd91
Aud3rk <sadykov.zaxar@mail.ru>
2021-11-15 13:27:44 +0300

    Исправил появление главного меню

--- Roguelike/Assets/Scripts/Roguelike/UI/MainMenu/MainMenu.cs
+++ Roguelike/Assets/Scripts/Roguelike/UI/MainMenu/MainMenu.cs
@@ -8,14 +8,16 @@ public class MainMenu : MonoBehaviour
     private GameObject _overlay;
     private GameObject _mainMenu;
     private GameObject MainScreen;
+    private GameObject _ui;
     private GameObject _player;
     void OnEnable()
     {
         _mainMenu = ResourseLoadService.GetResource<GameObject>("UI/Game/MainMenu/MainMenu");
         MainScreen = GameObject.Find("MainScreen");
+        _ui = GameObject.Find("UI");
         _mainMenu = Instantiate(_mainMenu);
-        _mainMenu.transform.SetParent(MainScreen.transform,false);
-            //1159.9  552.6
+        _mainMenu.transform.SetParent(_ui.transform,false);
+        //1159.9  552.6
     }
 
     public void StartButton()
