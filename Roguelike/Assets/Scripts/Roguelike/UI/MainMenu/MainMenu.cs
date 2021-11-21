b240f056816bacabad8995a25c026bb4d6113d7b
Aud3rk <sadykov.zaxar@mail.ru>
2021-11-21 09:58:25 +0300

    Попытки настроить UIService

--- Roguelike/Assets/Scripts/Roguelike/UI/MainMenu/MainMenu.cs
+++ Roguelike/Assets/Scripts/Roguelike/UI/MainMenu/MainMenu.cs
@@ -1,35 +1,47 @@
 
+using Core;
 using Core.Utils;
 using UnityEngine;
 using DG.Tweening;
+using Roguelike.UI;
+using Roguelike.UIService;
+using Roguelike.World;
 using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private UIService _UIService;
     private GameObject _overlay;
     private GameObject _mainMenu;
     private GameObject _mainScreen;
     private GameObject _ui;
     private GameObject _player;
+    [SerializeField]public GameWorld _gameWorld;
     void OnEnable()
     {
+        //_UIService = GameApplication.RequireService<UIService>();
         _mainMenu = ResourseLoadService.GetResource<GameObject>("UI/Game/MainMenu/MainMenu");
         _mainScreen = GameObject.Find("MainScreen");
         _ui = GameObject.Find("UI");
         _mainMenu = Instantiate(_mainMenu, _ui.transform, false);
+        _mainMenu.name = "MainMenu";
     }
 
     public void StartButton()
     {
-        _mainMenu = GameObject.Find("MainMenu(Clone)");
-        GameObject.FindWithTag("Menu").transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce);// тут я пытался сделать анимацю с помощью дотвина но не судьба
-        _mainMenu.SetActive(false);
-        _player = ResourseLoadService.GetResource<GameObject>("GameWorld/2DObject/Player/Player");
+        //_mainMenu = _UIService.RequaireObjectUIByName("MainMenu");
+        
+        GameObject.FindWithTag("Menu").transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.Flash);// тут я пытался сделать анимацю с помощью дотвина но не судьба
+        GameObject.FindWithTag("Menu").SetActive(false);
+        //_mainMenu.SetActive(false);
+        //_player = ResourseLoadService.GetResource<GameObject>("GameWorld/2DObject/Player/Player");
         GameObject _world = GameObject.Find("World");
-        _player = Instantiate(_player, _world.transform, false);
+        //_player = Instantiate(_player, _world.transform, false);
         _mainScreen = GameObject.Find("MainScreen");
         _overlay = ResourseLoadService.GetResource<GameObject>("UI/Game/Overlay/pfGameOverlay");
+        _mainScreen = _gameWorld.RequaireObjectByName("Player");
+        _mainScreen.SetActive(false);
         Instantiate(_overlay).transform.SetParent(_mainScreen.transform, false);
     }
 }
---
+++ Roguelike/Assets/Scripts/Roguelike/UI/UIService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Roguelike.UIService
+{
+    public class UIService : MonoBehaviour
+    {
+        private List<GameObject> _gameUIObjects;
+        
+        
+                private void Awake()
+                {
+                    FetchUIObjects();
+                }
+        
+                [NotNull]
+                public GameObject RequaireObjectUIByName(string objectName)
+                {
+                    GameObject go = _gameUIObjects.FirstOrDefault(o => o.name == objectName);
+                    if (go == null)
+                    {
+                        throw new NullReferenceException($"Объекта с именем {objectName} нет в интерфейсе");
+                    }
+        
+                    return go;
+                }
+        
+                [CanBeNull]
+                public GameObject GetParentObjectUIByName(string objectName)
+                {
+                    return _gameUIObjects.FirstOrDefault(o => o.name == objectName);
+                }
+        
+                public void AddGameObjectUI(GameObject gO, [CanBeNull] GameObject container = null,
+                    bool worldPositionStays = false)
+                {
+                    FetchUIObjects();
+                    Transform parentContainer = container == null ? transform : container.transform;
+                    Instantiate(gO).transform.SetParent(parentContainer, worldPositionStays);
+                }
+        
+                public void CreateObject(string reqObject, [CanBeNull] string parentObject)
+                {
+                    AddGameObjectUI(RequaireObjectUIByName(reqObject), GetParentObjectUIByName(parentObject));
+                }
+        
+                public void DestroyObject(string objectName)
+                {
+                    FetchUIObjects();
+                    Destroy(GetParentObjectUIByName(objectName));
+                    Debug.Log("Destroyed");
+                }
+        
+                public List<GameObject> GameUIObjects
+                {
+                    get { return _gameUIObjects; }
+                }
+        
+                public void FetchUIObjects()
+                {
+                    _gameUIObjects = gameObject.GetComponentsInChildren<Transform>(true).ToList().Select(t => t.gameObject)
+                        .ToList();
+                    Debug.Log(_gameUIObjects);
+                }
+        // написать тоже самое, что и GameWorld
+    }
+}
