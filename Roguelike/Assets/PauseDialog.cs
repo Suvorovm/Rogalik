08ad1f36b5158916e307c4722bf0bd256a718886
l0narl0ser <92228144+l0narl0ser@users.noreply.github.com>
2021-11-30 23:09:15 +0300

    Реализован диалог паузы.

--- Roguelike/Assets/PauseDialog.cs
+++ Roguelike/Assets/PauseDialog.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Core;
+using Core.UI;
+using Core.UI.Model;
+using Roguelike.Service;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PauseDialog : MonoBehaviour
+public class PauseDialog : MonoBehaviour, IDialog
 {
-    void Update()
+    public const string DIALOG_PATH = "UI/Game/Dialog/pfPauseDialog";
+    [SerializeField]
+    private Button _resumeButton;
+    [SerializeField]
+    private Button _menuButton;
+    private UIService _uiService;
+    private PauseService _pause;
+    public void Configure(IDialogModel dialogModel)
     {
-        
+        _uiService = GameApplication.RequireService<UIService>();
+        _resumeButton.onClick.AddListener(OnResumeButtonClick);
+        _menuButton.onClick.AddListener(OnMenuButtonClick);
+        _pause = GameApplication.RequireService<PauseService>();
+    }
+
+    private void OnMenuButtonClick()
+    {
+       GameApplication.Restart();
+       Debug.Log("Кто это читает, тот Егор" );
+    }
+
+    private void OnResumeButtonClick()
+    {
+        _uiService.HideDialog(gameObject);
+        _pause.ChangePauseState();
     }
 }
