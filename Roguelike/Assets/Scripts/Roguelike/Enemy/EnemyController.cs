7d4b579478b63e2ac315340169595885106eac02
Suvorovm <royming06@gmail.com>
2021-12-10 21:20:52 +0300

    code review in Commit

--- Roguelike/Assets/Scripts/Roguelike/Enemy/EnemyController.cs
+++ Roguelike/Assets/Scripts/Roguelike/Enemy/EnemyController.cs
@@ -9,37 +9,49 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private float _enemyHP = 50f;
-    [SerializeField]private Animator _animator;
+    [SerializeField] private Animator _animator;
     private GameWorld _gameWorld;
+
     private GameObject _enemy;
+
+    //private  EnemyHealthService _enemyService
     public void Awake()
     {
-        
-        _gameWorld=GameWorld.GameWorldInstance;
-        EnemyHealthService enemyService = GameApplication.RequireService<EnemyHealthService>();
+        _gameWorld = GameWorld.GameWorldInstance;
+        EnemyHealthService
+            enemyService =
+                GameApplication
+                    .RequireService<EnemyHealthService>(); // В поле класса, зачем постоянно дергать геймаппликешен _enemyService
         enemyService.OnTakeDamage += OnTakeDamage;
     }
 
     public void OnTakeDamage(string id, float damage)
     {
-        _enemy = GameWorld.GameWorldInstance.RequaireObjectByName($"{id}");
-        if ( _enemy.name==gameObject.name && _enemy!= null)
+        _enemy = GameWorld.GameWorldInstance.RequaireObjectByName($"{id}"); // зачем брать из мира ??? и зачем строку оборачивать в строку  $"{id}" - это тоже самое, что  
+        //GameWorld.GameWorldInstance.RequaireObjectByName(id);
+        if (_enemy.name == gameObject.name &&
+            _enemy != null) // gameObject.name == id Для этого и протаскивали сюда ID-к скрипт должен висеть там же где Id-к
+            //ID - к должен быть униклаьным
         {
             if (_enemyHP > 0)
             {
-               _enemyHP -= damage;
+                _enemyHP -= damage;
                 return;
             }
             else
             {
                 EnemyHealthService enemyService = GameApplication.RequireService<EnemyHealthService>();
-                enemyService.OnTakeDamage -= OnTakeDamage;
-                _gameWorld.DestroyObjectByName(_enemy.name);
+                enemyService.OnTakeDamage -= OnTakeDamage; // Для таких целей у Unity Существует "Дестурктор"
+                _gameWorld.DestroyObjectByName(_enemy.name); //gameObject.name
             }
 
             return;
         }
-        
     }
-    
-}
+
+    /*private void OnDestroy()
+     //Дестурктор
+    {
+        throw new NotImplementedException();
+    }*/
+} //Тыкай чаще ctrl + alt + l пробелы, переносы и т.п расставленны  хаотично, сложно читать
